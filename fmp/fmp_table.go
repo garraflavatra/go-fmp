f72d9510d40b6edede248190f510bcdc32584c7d
Romein van Buren <romein@vburen.nl>
2025-06-12 15:12:34 +0200

    Table looping (WIP)

--- fmp/fmp_table.go
+++ fmp/fmp_table.go
@@ -2,14 +2,43 @@ package fmp
 
 func (ctx *FmpFile) Tables() []*FmpTable {
 	tables := make([]*FmpTable, 0)
-	metaDict := ctx.Dictionary.get([]uint64{4, 1, 7})
-	if metaDict == nil {
-		return tables
+
+	for _, chunk := range ctx.Chunks {
+		if chunk.Key != 3 || chunk.Type != FMP_CHUNK_SIMPLE_KEY_VALUE {
+			continue
+		}
+
+		for _, chunk = range ctx.Chunks {
+			if chunk.Key != 16 {
+				continue
+			}
+
+			for _, chunk = range ctx.Chunks {
+				if chunk.Key != 5 {
+					continue
+				}
+
+				for tablePath, chunk := range ctx.Chunks {
+					if chunk.Key >= 128 {
+						continue
+					}
+
+					// Found a table!
+					println("Found one at", tablePath)
+				}
+			}
+		}
 	}
-	for _, meta := range *metaDict.Children {
-		name := decodeByteSeq(meta.Children.get([]uint64{16}).Value)
-		table := &FmpTable{Name: name}
-		tables = append(tables, table)
-	}
+
+	// metaDict := ctx.Dictionary.get([]uint64{4, 1, 7})
+	// if metaDict == nil {
+	// 	return tables
+	// }
+	// for _, meta := range *metaDict.Children {
+	// 	name := decodeByteSeq(meta.Children.get([]uint64{16}).Value)
+	// 	table := &FmpTable{Name: name}
+	// 	tables = append(tables, table)
+	// }
+
 	return tables
 }
